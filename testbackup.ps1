7d5d4dc94db26e8d5ec5df176fbfcfaa7ad2f31d
h1804 <jr@qubitsec.net>
2023-11-23 14:37:19 -0500

    signed commit added count

--- testbackup.ps1
+++ testbackup.ps1
@@ -1,5 +1,16 @@
 # Get the path to the Downloads directory
 $downloadsPath = "C:\Users\jpres\Downloads"
+# Get the files in the Downloads directory
+$downloadFiles = Get-ChildItem -Path $downloadsPath
+
+# Count the number of files
+$fileCount = $downloadFiles.Count
+
+# Calculate the total file size
+$totalFileSize = ($downloadFiles | Measure-Object -Property Length -Sum).Sum
+
+$fileCount
+$totalFileSize
 
 # Get the current date and subtract 90 days
 $cutOffDate = (Get-Date).AddDays(-90)
@@ -8,9 +19,13 @@ $cutOffDate = (Get-Date).AddDays(-90)
 $filesToDelete = Get-ChildItem -Path $downloadsPath | Where-Object { $_.LastWriteTime -lt $cutOffDate }
 
 # Delete the files
-$filesToDelete | Remove-Item -Force
+#$filesToDelete | Remove-Item -Force
 
 # Save the list of deleted files to a text file on the desktop
 $desktopPath = "C:\Users\jpres\OneDrive\Desktop"
 $deletedFilesPath = Join-Path -Path $desktopPath -ChildPath "DeletedFiles.txt"
 $filesToDelete.FullName | Out-File -FilePath $deletedFilesPath
+
+# Save the file count to a text file on the desktop
+$fileCountPath = Join-Path -Path $desktopPath -ChildPath "psFileCount.txt"
+$fileCount | Out-File -FilePath $fileCountPath
